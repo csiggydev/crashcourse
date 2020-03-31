02932795bb118683577cd9986ed77e3034cc381c
ciggydev <csegalas@gmail.com>
2020-03-30 21:17:00 -0700

    exercise 3-4

---
+++ 3-4_guestlist.py
@@ -0,0 +1,12 @@
+# 3-4 Guest List
+
+sir = ['Ben Kingsley', 'Richard Attenborough', 'Sean Connery']
+
+message1 = f"I humbly invite you, {sir[0].title()}, to dinner this evening."
+print(message1)
+
+message2 = f"Additionally, i'd like to welcome you, {sir[1].title()}--to Jurras...dinner!"
+print(message2)
+
+message3 = f"{sir[2].title()}, you're invited as well."
+print(message3)
